51e7cbc32ffd79b644bb0e436d436257799d4b6d
ltrii <lelandtr@gmail.com>
2019-02-13 12:29:44 -0800

    Post router done and some work on user router

--- routers/post_router.js
+++ routers/post_router.js
@@ -14,6 +14,76 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    if(!req.body.text) {
+        res.status(400).json({error: "Please provide text for the post."});
+        return;
+    }
+
+    try {
+        const postFull = {
+            text: req.body.text,
+            user_id: req.body.user_id
+        };
+        let newId = await db.insert(postFull);
+        let newPost = await db.getById(newId.id);
+        res.status(201).json(newPost);
+    } catch(error) {
+        res.status(500).json({error: "There was an error while saving the post to the database"});
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.getById(req.params.id);
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+        
+    } catch(error) {
+        res.status(500).json({error: "The post information could not be retrieved."})
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+      const count = await db.remove(req.params.id);
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been nuked' });
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the post',
+      });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    if(!req.body.text) {
+        res.status(400).json({error: "Please provide text for the post."});
+        return;
+    }
+    
+    try {
+      const post = await db.update(req.params.id, req.body);
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the post',
+      });
+    }
+});
 
 
 module.exports = router;
--- routers/user_router.js
+++ routers/user_router.js
@@ -5,6 +5,27 @@ const db = require('../data/helpers/userDb');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        let users = await db.get();
+        res.status(200).json(users);
+    } catch(error) {
+        res.status(500).json({error: "The users information could not be retrieved."});
+    }
+});
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.getById(req.params.id);
+        if(post.length > 0) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+        }
+        
+    } catch(error) {
+        res.status(500).json({error: "The user information could not be retrieved."})
+    }
+});
 
 module.exports = router;
